26d216993a80a685e0e8fc550806d9566a3e565a
Vergara <h.vergara@accenture.com>
2022-08-24 00:37:26 -0300

    Actualizamos ejemplo

--- 01-jerarquia-de-componentes/src/App.js
+++ 01-jerarquia-de-componentes/src/App.js
@@ -5,8 +5,12 @@ import Padre3A from './components/03-A-padre-hijos-nietos';
 import Padre3B from './components/03-B-padre-hijos-nietos';
 import Padre4 from './components/04-hijo-a-hijo';
 import Padre5 from './components/05-captura-de-evento';
+import FormularioSimple from './components/06-formulario-simple';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState("My Username");
+  const [password, setPassword] = useState("MySuperSecretPassword");
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +19,16 @@ function App() {
       {/* <Padre3A/> */}
       {/* <Padre3B/> */}
       {/* <Padre4/> */}
-      <Padre5/>
+      {/* <Padre5/> */}
+      {/* <h3>Ejemplo 6:</h3>
+      <h4>Username: {username}</h4>
+      <h4>Password: {password}</h4>
+      <FormularioSimple 
+      username = {username}
+      password = {password}
+      userNameHook={setUsername} 
+      passwordHook={setPassword}/> */}
+
       </header>
     </div>
   );
