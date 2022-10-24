b580fb276cd5b129e78f9627928e37254819727e
Ale <103283338+alevita1980@users.noreply.github.com>
2022-10-24 09:41:25 -0300

    Delete 02-ejemplo-practico directory

--- 02-ejemplo-practico/src/pages/logout.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const Logout = (props) => {
-  const { auth, setAuthHook } = props;
-  return <Navigate to="/login">{setAuthHook(false)}</Navigate>;
-};
-export default Logout;
